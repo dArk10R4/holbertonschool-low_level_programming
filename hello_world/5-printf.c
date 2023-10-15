afda75c9cb91c50ff37c9363c655f8e0c7fc2a95
dArk10R4 <goylerov04@gmail.com>
2023-10-15 12:04:04 -0700

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ * void: First operand
+ *
+ *
+ * Description: Prints using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
