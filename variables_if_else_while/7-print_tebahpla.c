43e26eb39456a8705fb638d0ab81b2e46f5e17d0
dArk10R4 <goylerov04@gmail.com>
2023-10-17 04:57:42 -0700

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alpabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
