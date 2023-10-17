8b0afd6446fef9a279136a6e720552a14770be3d
dArk10R4 <goylerov04@gmail.com>
2023-10-17 05:48:39 -0700

    10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alpabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+
+
+			putchar(i);
+			putchar(j);
+			if (i != '9' && j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
