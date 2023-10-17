086c202f0f2396290688dd53b85bc88bc4282356
dArk10R4 <goylerov04@gmail.com>
2023-10-17 05:54:18 -0700

    10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -18,11 +18,13 @@ int main(void)
 
 			putchar(i);
 			putchar(j);
-			if (i != '8' && j != '9')
+			if (i == '8' && j == '9')
 			{
-				putchar(',');
-				putchar(' ');
+				continue;
 			}
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
