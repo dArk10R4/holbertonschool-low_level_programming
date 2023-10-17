a2ffb7e6a11369f845544832dbd549559ba68f91
dArk10R4 <goylerov04@gmail.com>
2023-10-17 05:50:34 -0700

    10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '8'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = i; j <= '9'; j++)
 		{
 
 
 			putchar(i);
 			putchar(j);
-			if (i != '9' && j != '9')
+			if (i != '8' && j != '9')
 			{
 				putchar(',');
 				putchar(' ');
