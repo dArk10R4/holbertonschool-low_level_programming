00ebd37f3194e91e8ab626a535a3fa8a19a24baf
dArk10R4 <goylerov04@gmail.com>
2023-10-19 11:53:50 -0700

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@ void jack_bauer(void)
 
 	for (i = 0; i < 2400; i++)
 	{
-		_putchar(i / 1000);
-		_putchar((i / 100) % 10);
+		_putchar(i / 1000 + 48);
+		_putchar((i / 100) % 10 + 48);
 		_putchar(':');
-		_putchar((i / 10) % 10);
-		_putchar(i % 10);
+		_putchar((i / 10) % 10 + 48);
+		_putchar(i % 10 + 48);
 		_putchar('\n');
 	}
 }
