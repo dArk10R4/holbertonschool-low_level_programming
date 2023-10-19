e8e771d6425271ae3e5a73b35d944c30ae1357f9
dArk10R4 <goylerov04@gmail.com>
2023-10-19 10:46:30 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	putchar('\n');
+
+	return;
+}
