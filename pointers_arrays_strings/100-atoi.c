e8c59621829422402ad12ce363f25eafa8c70f1b
dArk10R4 <goylerov04@gmail.com>
2023-10-26 07:56:11 -0700

    ptr

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,21 @@ int _atoi(char *s)
 	unsigned int num = 0;
 	short int sign = 1;
 	int len = 0;
+	short int start = 0;
 
 	while (*(s + len))
 	{
+		if (start && (*(s + len) < '0' || *(s + len) > '9'))
+			break;
+
 		if (*(s + len) == '-')
 			sign *= -1;
 
 		if (*(s + len) >= '0' && *(s + len) <= '9')
+		{
+			start = 1;
 			num = num * 10 + *(s + len) - 48;
+		}
 		len++;
 	}
 
