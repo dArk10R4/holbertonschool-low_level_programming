262813535b98f404113aef69d23a1a34c39d28b9
dArk10R4 <goylerov04@gmail.com>
2023-10-27 07:16:46 -0700

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,6 +8,8 @@ char *cap_string(char *s)
 {
 	int i = 1;
 	int j;
+	char arr[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '\"', '(',
+			')', '{', '}'};
 
 	if (*s != '\0' && *s >= 'a' && *s <= 'z')
 		*s = *s - 'a' + 'A';
@@ -20,13 +22,14 @@ char *cap_string(char *s)
 		{
 			for (j = 0; j < 13; j++)
 			{
-				if (*(s + i - 1) == {}[j])
+				if (*(s + i - 1) == arr[j])
 				{
 					*(s + i) = *(s + i) - 'a' + 'A';
 					break;
 				}
 			}
 		}
+		i++;
 	}
 
 	return (s);
