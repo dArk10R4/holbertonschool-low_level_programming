2f6a661acc410ed888a8c81f8372bd6d9e8cc453
dArk10R4 <goylerov04@gmail.com>
2023-11-11 05:13:52 -0800

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,11 +10,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ans;
+	int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ans = malloc(nmemb * size);
+
+	if (ans == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ans[i] = 0;
 
 	return (ans);
 }
