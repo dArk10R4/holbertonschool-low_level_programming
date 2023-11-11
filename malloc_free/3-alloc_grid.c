b2cd1dab6fec01cd7ba90f5b3e4735330bea42bc
dArk10R4 <goylerov04@gmail.com>
2023-11-11 02:17:07 -0800

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,11 +9,11 @@
  */
 int **alloc_grid(int width, int height)
 {
-	if (width == 0 || height == 0)
-		return (NULL);
-
 	int i;
 	int **ans;
+
+	if (width == 0 || height == 0)
+		return (NULL);
 
 	ans = (int **)malloc(sizeof(int *) * height);
 
