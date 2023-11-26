77162ff0423e89f76f99c0b887727d9ae2a4713e
dArk10R4 <goylerov04@gmail.com>
2023-11-26 09:48:30 -0800

    j

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 
 /**
  * array_iterator - array iterator
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef F_P_H
 #define F_P_H
 void print_name(char *name, void (*f)(char *));
