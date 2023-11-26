c841608bbcf634ea4f37fdbfef2b39d167752eb9
dArk10R4 <goylerov04@gmail.com>
2023-11-26 09:45:24 -0800

    j

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - array iterator
+ * @array: int *
+ * @size: size_t
+ * @action: action
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (action == NULL)
+		return;
+
+	for (; i < size; i++)
+		action(array[i]);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef F_P_H
 #define F_P_H
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
