1b0f32d62866bc7256bbb49dc506a9ebb98705d8
dArk10R4 <goylerov04@gmail.com>
2023-12-10 03:54:08 -0800

    1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "lists.h"
+
 
 /**
  * dlistint_len - finds len
@@ -10,7 +12,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
 
-	while (h && len++)
+	while (h && ++len)
 		h = h->next;
 
 	return (len);
