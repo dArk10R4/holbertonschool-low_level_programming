009b1b8c5ce63929cace7d2297cb35617adb3925
dArk10R4 <goylerov04@gmail.com>
2023-12-10 03:21:38 -0800

    0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints
+ * @h: head
+ *
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
