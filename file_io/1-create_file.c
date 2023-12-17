90347bec528c042faa9541cbe4c77447bebb6290
dArk10R4 <goylerov04@gmail.com>
2023-12-17 07:08:43 -0800

    f

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: absolute/relative path to a file
+ * @text_content: content to write into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, size;
+	ssize_t wt;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
+	for (size = 0; text_content[size] != '\0'; size++)
+		;
+
+	wt = write(file, text_content, size);
+
+	if (wt == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
